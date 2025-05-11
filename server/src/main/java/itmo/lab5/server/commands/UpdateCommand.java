26d803e6f77c1380ce7dcb0444bdc8f2fb46930d
ZueffC <zueffc@gmail.com>
2025-05-12 01:41:27 +0300

    The new version

--- server/src/main/java/itmo/lab5/server/commands/UpdateCommand.java
+++ server/src/main/java/itmo/lab5/server/commands/UpdateCommand.java
@@ -9,6 +9,13 @@ import java.util.HashMap;
  */
 public class UpdateCommand {
     public static String execute(int id, Flat newFlat, HashMap<Integer, Flat> collection) {
-         
+        var oldFlat = collection.getOrDefault(id, null);
+        if (oldFlat == null)
+            return "There is not flat with such id!";
+        
+        if(newFlat.getName().length() > 0)
+            oldFlat.setName(newFlat.getName()); 
+        
+        return null;
    }
 }
